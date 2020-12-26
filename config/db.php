a6244febaba9e898245819923d831f8ea1a8e201
harsha099 <44014170+harsha099@users.noreply.github.com>
2020-12-26 14:38:36 +0530

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "harsha";
+$databaseName = "time_table";
+
+// connect to mysql database
+
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
---
+++ generatedtimetable.php
@@ -0,0 +1,55 @@
+<?php
+require('./config/db.php');
+$selectedcourses = $_POST;
+$timetable=array();
+foreach($selectedcourses as $key => $value){
+  $query = "select * from timetable where Faculty_Name='$value' and Course_Code='$key'";
+  $result = mysqli_query($connect,$query);
+  while($row = mysqli_fetch_array($result)){
+    array_push($timetable,$row);
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css"/>
+    <title>List Of Courses</title>
+  </head>
+  <body>
+    <section class="container mt-2">
+      <h2>Generated Time Table:</h2>
+      <table class="table">
+        <thead>
+            <tr>
+              <th>S.NO</th>
+              <th>Course_Code</th>
+              <th>Course_TiTle</th>
+              <th>Section</th>
+              <th>Faculty_Name</th>
+              <th>Days</th>
+              <th>Hours</th>
+            </tr>
+        </thead>
+        <tbody>
+              <?php for($i=0;$i<count($timetable);$i++):?>
+                <tr>
+                  <td><?php echo $i+1; ?>
+                  <td><?php echo $timetable[$i]['Course_Code'];?></td>
+                  <td><?php echo $timetable[$i]['Course_Title'];?></td>
+                  <td><?php echo $timetable[$i]["Section"] ?></td>
+                  <td><?php echo $timetable[$i]["Faculty_Name"] ?></td>
+                  <td><?php echo $timetable[$i]["Days"] ?></td>
+                  <td><?php echo $timetable[$i]["Hour"] ?></td>
+                </tr>
+              <?php endfor; ?>
+        </tbody>
+      </table>
+    </section>
+  </body>
+  <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</html>
