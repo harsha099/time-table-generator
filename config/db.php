f972d1789d6d9d4d5667f0878f67b46d65fd08e2
harsha099 <44014170+harsha099@users.noreply.github.com>
2021-01-30 08:31:46 +0530

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "harsha";
-$databaseName = "time_table";
-
-// connect to mysql database
-
-$connect = mysqli_connect($hostname, $username, $password, $databaseName);
