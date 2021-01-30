f280a6246e5b63b208c0b4efc71174987d12f680
harsha099 <44014170+harsha099@users.noreply.github.com>
2021-01-30 08:34:09 +0530

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "harsha";
+$databaseName = "time_table";
+
+
+
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
